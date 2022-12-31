6001525797c940d6d2204c66338d0a78e5c76664
behrooz3500 <behrooz3500@yahoo.com>
2022-12-31 07:28:49 +0330

    add posts and category base models

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """A class to define different categories for posts"""
+    name = models.CharField(max_length=99)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    """A class to define posts in blog app"""
+    title = models.CharField(max_length=250)
+    image = models.ImageField(null=True, blank=True)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
